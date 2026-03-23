b3a065cd46b6b1c7a9cd527bf44e69868a64b855
2001aswin89 <2001aswin89@gmail.com>
2026-03-23 13:43:38 +0530

    chore: initial commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const logger = require('./middleware/logger');
+const { tasks } = require("./data/tasks");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
 
 app.listen(port, (err) => {
     if (err) {
