f76f76cdb1fe589dc00a7a8716027c99fb1ec5bb
2001aswin89 <2001aswin89@gmail.com>
2026-03-24 10:35:14 +0530

    feat: priority field added.
    feat: added filter query based on completed in get all tasks api

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { getAllTasks, getTasksById, CreateTask, updateTask, deleteTask } = require("../controllers/taskControllers");
+const { getAllTasks, getTasksById, createTask, updateTask, deleteTask, getTasksByPriority } = require("../controllers/taskControllers");
 
 router.get("/", getAllTasks);
 router.get("/:id", getTasksById);
-router.post("/", CreateTask);
+router.post("/", createTask);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
+router.get("/priority/:level", getTasksByPriority);
 
 module.exports = router;
